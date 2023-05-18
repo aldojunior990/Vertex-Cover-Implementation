59076ab0750271fd89e6786b20c32340185a4e60
TainaIsabela <tainaisabela9@gmail.com>
2023-05-18 17:00:36 -0300

    Correção da primeira parte + inicio da 2º

--- vertex-cover.cpp
+++ vertex-cover.cpp
@@ -2,9 +2,13 @@
 #include <vector>
 #include <utility>
 #include <algorithm>
+#include <unordered_set>
 
 using namespace std;
 
+/*
+----------Primeira parte----------
+*/
 bool isVertexCover(vector<pair<int, int>>& I, vector<int>& S) {
     // Verifica cada aresta em I
     for (const auto& edge : I) {
@@ -13,12 +17,44 @@ bool isVertexCover(vector<pair<int, int>>& I, vector<int>& S) {
         
         // Verifica se nem u nem v estão presentes em S
         if (find(S.begin(), S.end(), u) == S.end() && find(S.begin(), S.end(), v) == S.end()) {
-            cout << "Está é uma solução válida para a instância I do problema Vertex-Cover." << endl;
+            cout << "Está não é uma solução válida para a instância I do problema Vertex-Cover." << endl;
             return false;
         }
     }
-    cout << "Está não é uma solução válida para a instância I do problema Vertex-Cover." << endl;
+    cout << "Está é uma solução válida para a instância I do problema Vertex-Cover." << endl;
     return true;
+}
+
+/*
+----------Segunda parte----------
+*/
+
+vector<int> findIndependentSet(vector<pair<int, int>>& I) {
+    unordered_set<int> independentSet; // Conjunto para armazenar os vértices independentes
+    
+    // Verifica cada aresta em I
+    for (const auto& edge : I) {
+        int u = edge.first;
+        int v = edge.second;
+        
+        // Adiciona u e v ao conjunto se não estiverem presentes
+        if (independentSet.find(u) == independentSet.end()) {
+            independentSet.insert(u);
+        }
+        if (independentSet.find(v) == independentSet.end()) {
+            independentSet.insert(v);
+        }
+    }
+    
+    vector<int> result(independentSet.begin(), independentSet.end()); // Converte o conjunto para um vetor
+    
+    cout << "Conjunto Independente: ";
+    for (const auto& vertex : result) {
+        cout << vertex << " ";
+    }
+    cout << endl;
+
+    return result;
 }
 
 int main() {
@@ -29,6 +65,9 @@ int main() {
     
     isVertexCover(I, S);
     isVertexCover(I, S2);
+    vector<int> independentSet = findIndependentSet(I); // Conjunto independente de I
+
+    
     
     return 0;
 }
