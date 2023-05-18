2738ab75b7e59aacc73b6b8e6d2561750ea6f52b
TainaIsabela <tainaisabela9@gmail.com>
2023-05-18 16:48:51 -0300

    Adicionando primeira parte

---
+++ vertex-cover.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <algorithm>
+
+using namespace std;
+
+bool isVertexCover(vector<pair<int, int>>& I, vector<int>& S) {
+    // Verifica cada aresta em I
+    for (const auto& edge : I) {
+        int u = edge.first;
+        int v = edge.second;
+        
+        // Verifica se nem u nem v estão presentes em S
+        if (find(S.begin(), S.end(), u) == S.end() && find(S.begin(), S.end(), v) == S.end()) {
+            cout << "Está é uma solução válida para a instância I do problema Vertex-Cover." << endl;
+            return false;
+        }
+    }
+    cout << "Está não é uma solução válida para a instância I do problema Vertex-Cover." << endl;
+    return true;
+}
+
+int main() {
+    // Exemplo de uso
+    vector<pair<int, int>> I = {{1, 2}, {2, 3}, {3, 4}}; // Instância do problema Vertex-Cover
+    vector<int> S = {1, 3}; // Solução proposta para I (Válido)
+    vector<int> S2 = {1, 4}; // Solução proposta para I (Inválido)
+    
+    isVertexCover(I, S);
+    isVertexCover(I, S2);
+    
+    return 0;
+}
